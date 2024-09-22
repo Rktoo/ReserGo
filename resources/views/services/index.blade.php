af560eb16ca16e3fc91819f1221f756197f6b621
Rktoo <heryinjaka72@gmail.com>
2024-09-22 09:39:41 +0300

    Add more shadow to the services container

--- resources/views/services/index.blade.php
+++ resources/views/services/index.blade.php
@@ -1,18 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-    <div class="flex flex-col sm:flex-row justify-between items-start sm:items-center mx-2">
-    <h1 class="text-2xl max-sm:text-xl mt-2 font-semibold">Services</h1>
-    <a href="{{ route('services.create') }}" class="inline-block mt-2 py-2 px-4 max-sm:text-sm text-nowrap bg-blue-600 text-white rounded-md hover:bg-blue-700">Ajouter un Service</a>
-</div>
-    <div class="mt-4">
-        @foreach($services as $service)
-            <div class="p-4 bg-white rounded shadow-sm mb-4">
-                <h3 class="text-lg font-medium">{{ $service->name }}</h3>
-                <p>{{ $service->description }}</p>
-            </div>
-        @endforeach
+    <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <div class="flex flex-col sm:flex-row justify-between items-start sm:items-center mx-2">
+            <h1 class="text-2xl max-sm:text-xl mt-2 font-semibold">Services</h1>
+            <a href="{{ route('services.create') }}"
+                class="inline-block mt-2 py-2 px-4 max-sm:text-sm text-nowrap bg-blue-600 text-white rounded-md hover:bg-blue-700">Ajouter
+                un Service</a>
+        </div>
+        <div class="mt-4">
+            @foreach ($services as $service)
+                <div class="p-4 bg-white rounded shadow-md mb-4">
+                    <h3 class="text-lg font-medium">{{ $service->name }}</h3>
+                    <p>{{ $service->description }}</p>
+                </div>
+            @endforeach
+        </div>
     </div>
-</div>
 @endsection

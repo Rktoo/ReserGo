bdafaad0e3f359737b2eaf2f305d663c33168bf9
Rktoo <heryinjaka72@gmail.com>
2024-09-22 09:27:45 +0300

    Add  bg gradient

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,12 +1,15 @@
 <nav class="px-2">
-    <div class="bg-blue-600 p-4 container mx-auto flex justify-between">
-        <div class=""> 
-            <a href="{{ route('services.index') }}" class="bg-blue-600 hover:bg-blue-300 text-white font-semibold p-4">Services</a>
-            <a href="{{ route('reservations.index') }}" class="bg-blue-600 hover:bg-blue-300 text-white font-semibold p-4">Réservations</a>
+    <div class="bg-blue-600 border p-4 container mx-auto flex justify-between">
+        <div class="">
+            <a href="{{ route('services.index') }}"
+                class="bg-blue-600 hover:bg-blue-300 text-white font-semibold p-4">Services</a>
+            <a href="{{ route('reservations.index') }}"
+                class="bg-blue-600 hover:bg-blue-300 text-white font-semibold p-4">Réservations</a>
         </div>
         <div>
             <a href="{{ route('about') }}" class="bg-blue-600 hover:bg-blue-300 text-white font-semibold p-4">À propos</a>
-            <a href="{{ route('contact') }}" class="bg-blue-600 hover:bg-blue-300 text-white font-semibold p-4">Contact</a>
+            <a href="{{ route('contact') }}"
+                class="bg-blue-600 hover:bg-blue-300 text-white font-semibold p-4">Contact</a>
         </div>
     </div>
 </nav>

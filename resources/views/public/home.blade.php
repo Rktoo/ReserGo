8fe8e438121b4296630d72bc44b1ebf8dd7a45f5
Rktoo <heryinjaka72@gmail.com>
2024-09-23 16:47:10 +0300

    Add image preview to the services

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -9,14 +9,18 @@
         <h2 class="text-2xl font-semibold mb-4">Nos Services</h2>
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             @foreach ($services as $service)
-                <div class="flex flex-col justify-between p-4 bg-white shadow rounded-lg">
-                    <h3 class="text-lg font-bold">{{ $service->name }}</h3>
-                    <p class="text-gray-600">{{ $service->description }}</p>
-                    <div class="flex flex-col justify-end">
+                <div class="relative flex flex-col justify-between p-4 bg-white shadow rounded-lg overflow-hidden">
+                    <h3 class="text-lg font-bold z-20">{{ $service->name }}</h3>
+                    <p class="text-gray-600 z-20">{{ $service->description }}</p>
+                    <div class="flex flex-col justify-end z-20">
                         <p class="text-blue-600 font-semibold mt-2">{{ number_format((float) $service->price, 2) }} €</p>
                         <a href="{{ route('reservations.create', $service->id) }}"
                             class="text-blue-500 mt-2 inline-block">Réserver ce service</a>
                     </div>
+                    @if ($service->image_url)
+                        <img src="{{ $service->image_url }}" alt="image for {{ $service->name . ' ' }}service"
+                            class="absolute top-0 left-1/2 w-full opacity-40 bg-cover z-10">
+                    @endif
                 </div>
             @endforeach
         </div>

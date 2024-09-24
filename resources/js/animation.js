990f6d78e1bf932bfeb2d890592133e6304c7870
Rktoo <heryinjaka72@gmail.com>
2024-09-24 17:12:07 +0300

    Update animation

--- resources/js/animation.js
+++ resources/js/animation.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
             { opacity: 0, y: -200 },
             {
                 opacity: 1, y: 0, duration: 1, ease: "power1.out", onComplete: () => {
-                    afterTitle()
+                    afterTitle();
                 }
             }
         );

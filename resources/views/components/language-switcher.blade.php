13b7b794c1a3da1a98416de80c24bf38bcf9372c
Rktoo <heryinjaka72@gmail.com>
2024-09-25 10:36:09 +0300

    Add flag image for each language

--- resources/views/components/language-switcher.blade.php
+++ resources/views/components/language-switcher.blade.php
@@ -1,15 +1,24 @@
 <div>
-    <div>
+    <div class="flex justify-start items-center space-x-2">
         <form action="{{ route('change.language', ['lang' => 'fr']) }}" method="GET" class="inline">
-            <button type="submit" @if (session('locale') === 'fr') class="font-bold" @endif>FR</button>
+            <button type="submit"
+                class="@if (session('locale') === 'fr') scale-105 @else opacity-60 @endif flex justify-center items-center hover:opacity-100 transition-all duration-200 ease-in-out">
+                <img src="{{ asset('images/flags/fr.png') }}" alt="drapeau français" class="w-7 h-[1.10rem]" />
+            </button>
         </form>
 
         <form action="{{ route('change.language', ['lang' => 'en']) }}" method="GET" class="inline">
-            <button type="submit" @if (session('locale') === 'en') class="font-bold" @endif>EN</button>
+            <button type="submit"
+                class="@if (session('locale') === 'en') scale-105 @else opacity-60 @endif flex justify-center items-center hover:opacity-100 transition-all duration-200 ease-in-out">
+                <img src="{{ asset('images/flags/en.png') }}" alt="drapeau anglais" class="w-7 h-7" />
+            </button>
         </form>
 
         <form action="{{ route('change.language', ['lang' => 'mg']) }}" method="GET" class="inline">
-            <button type="submit" class="@if (session('locale') === 'mg') font-bold @endif">MG</button>
+            <button
+                type="submit"class="@if (session('locale') === 'mg') scale-105 @else opacity-60 @endif flex justify-center items-center hover:opacity-100 transition-all duration-200 ease-in-out">
+                <img src="{{ asset('images/flags/mg.png') }}" alt="drapeau malagasy" class="w-7 h-7" />
+            </button>
         </form>
     </div>
     @if (session('success'))
